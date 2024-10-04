da29aa38cb8f57067cf2d44fb728c354437b2cab
Sravan Surya <vsravansurya@gmail.com>
2024-10-04 09:17:10 +0530

    logicalOperation on javaScript

---
+++ part1/logical.js
@@ -0,0 +1,15 @@
+/*
+&&->and
+ ||->or
+!->reverse
+*/
+
+let isLoggedin = true
+let ispaid=true
+console.log(isLoggedin && ispaid);
+
+let isEmailUser=true
+let isGoogleUser=false
+
+console.log(isEmailUser || isGoogleUser);
+
