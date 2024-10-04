cf0e7e56f2cb908207c9b3c3888c97f6b64584ea
Sravan Surya <vsravansurya@gmail.com>
2024-10-04 09:16:31 +0530

    operation on javascript

---
+++ part1/operation.js
@@ -0,0 +1,30 @@
+let score = 103
+let bonous = 255
+let totalScore=score + bonous
+
+let addition = 4+5
+
+
+let subtract = 9-3
+let mult = 3*5
+let div =8/4;
+let remainder =9%2
+let expo=2**4
+
+myScore =110
+myScore++
+
+let credits=56;
+credits--
+
+// Comparsion operation
+
+let num1=3
+let num2=3
+let num3=6
+
+console.log(num1==num2);//true
+console.log(num1!=num2);//true
+console.log(num1>num3);//false
+console.log(num1<num3);//true
+
