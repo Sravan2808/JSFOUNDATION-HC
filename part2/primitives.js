7a4efcc56891f06e3a9d9947cb4a9175f9827b1d
Sravan Surya <vsravansurya@gmail.com>
2024-10-06 10:08:50 +0530

    primitives in javaScript

---
+++ part2/primitives.js
@@ -0,0 +1,39 @@
+// Number
+
+let balance = 120
+let anotherBalance = new Number(120)
+
+// console.log(anotherBalance);
+// console.log(anotherBalance.valueOf());
+
+// console.log(typeof(balance));
+// console.log(typeof(anotherBalance));
+
+//boolean
+let isActive = true
+let isReallyActive = new Boolean(true) //not
+
+//null and undefine
+
+let firstname;
+console.log(firstname);
+
+//string
+let myString = "hello"
+let myString1 = 'hola'
+let username = 'hitesh'
+
+let oldGreet = myString + "hitesh"
+console.log(oldGreet)
+
+let greetMessage = `Hello ${username} ! `
+let demoOne = `Value is ${2 * 2}`
+console.log(greetMessage);
+// console.log(demoOne);
+
+let sm1 = Symbol("hitesh")
+let sm2 = Symbol("hitesh")
+
+console.log(sm1 == sm2);
+
+
