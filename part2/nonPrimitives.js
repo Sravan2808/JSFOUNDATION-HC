32c979b0bedaa3628945741f94e4ef0395cd8b54
Sravan Surya <vsravansurya@gmail.com>
2024-10-06 16:36:44 +0530

    Non-Primitives in Js

---
+++ part2/nonPrimitives.js
@@ -0,0 +1,28 @@
+let username = {
+    firstname : "Sravan",
+    isLoggedin : true,
+}
+username.lastname = "Surya"
+console.log(username.firstname);
+console.log(username.lastname);
+console.log(username);
+console.log(typeof(username));
+
+let today = new Date();
+console.log(today);
+console.log(today.getDate());
+
+// Array
+
+let anotherUser = ["sravan",true];
+console.log(anotherUser[0]);
+console.log(anotherUser[1]);
+
+let isValue = "false"
+console.log(typeof Number(isValue)+1);
+console.log(Number(null));
+console.log(Number(undefined));
+
+
+
+
