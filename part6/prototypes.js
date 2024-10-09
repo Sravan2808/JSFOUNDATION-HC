46afe034b7a7427f5cf1576bc60247aac6e944c6
Sravan Surya <vsravansurya@gmail.com>
2024-10-09 13:54:59 +0530

    A masterclass on prototypes in javascript

---
+++ part6/prototypes.js
@@ -0,0 +1,20 @@
+let computer = {cpu:12}
+let lenovo = {
+    screen:"HD",
+    __proto__:computer,
+}
+let tomHardware = {}
+
+// console.log(`lenovo `,lenovo.__proto__);
+
+
+let genericCar = {
+    tyres:4
+}
+let tesla = {
+    drive:"AI"
+}
+Object.setPrototypeOf(tesla,genericCar)
+
+console.log(`tesla ` ,Object.getPrototypeOf(tesla));
+console.log(`genericCar ` ,genericCar);
